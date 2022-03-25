00b4c1fe8d4bd3c4b90af4786aa7a459df4460d3
Mohamed Gaballah <64907562+mohamedgaballah99@users.noreply.github.com>
2022-03-25 23:25:21 +0200

    Update login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -30,7 +30,7 @@ export default {
   },
   methods: {
       async login(){
-           let result = await axios.get(`https://my-json-server.typicode.com/mohamedgaballah99/resturantapi/users?email=${this.email}&password=${this.password}`)
+           let result = await axios.get(`http://localhost:3000/users?email=${this.email}&password=${this.password}`)
             console.log(result);
         if (result.status == 200 && result.data.length > 0) {
             localStorage.setItem('user-info',JSON.stringify(result.data[0]))
